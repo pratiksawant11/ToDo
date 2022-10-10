fea6452a2cd6592f353bf4f3aec69d30097595c9
pratiksawant11 <pratiksawant0011@gmail.com>
2022-10-10 17:32:48 +0530

    abc

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6" >
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe ipsa ab voluptatibus odit nam deleniti accusantium totam excepturi iusto ut consequuntur ad obcaecati necessitatibus, enim fugit, ea officia in. Consequatur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe ipsa ab voluptatibus odit nam deleniti accusantium totam excepturi iusto ut consequuntur ad obcaecati necessitatibus, enim fugit, ea officia in. Consequatur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe ipsa ab voluptatibus odit nam deleniti accusantium totam excepturi iusto ut consequuntur ad obcaecati necessitatibus, enim fugit, ea officia in. Consequatur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe ipsa ab voluptatibus odit nam deleniti accusantium totam excepturi iusto ut consequuntur ad obcaecati necessitatibus, enim fugit, ea officia in. Consequatur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe ipsa ab voluptatibus odit nam deleniti accusantium totam excepturi iusto ut consequuntur ad obcaecati necessitatibus, enim fugit, ea officia in. Consequatur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe ipsa ab voluptatibus odit nam deleniti accusantium totam excepturi iusto ut consequuntur ad obcaecati necessitatibus, enim fugit, ea officia in. Consequatur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe ipsa ab voluptatibus odit nam deleniti accusantium totam excepturi iusto ut consequuntur ad obcaecati necessitatibus, enim fugit, ea officia in. Consequatur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe ipsa ab voluptatibus odit nam deleniti accusantium totam excepturi iusto ut consequuntur ad obcaecati necessitatibus, enim fugit, ea officia in. Consequatur?</p>
   </div>
 </template>
